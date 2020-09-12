0204998b1253f82b287741c77d533ebcaacf392d
Prometheo <olaoyejnr@gmail.com>
2020-09-12 05:28:46 +0100

    finished the task

--- user_registration/custom_endpoint/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views.register import CreateUser
-from oscarapi.urls import urlpatterns as og
-
-urlpatterns = [
-    path("register/", CreateUser.as_view(), name="create_user"),
-] + og
-
-
